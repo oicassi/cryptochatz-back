27c3bb3515b3f8044b137b7b5b5a7aa8beda543e
Cassiano Kruchelski Vidal <casst@protonmail.com>
2020-04-26 18:46:10 -0300

    adjust hoster change

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ io.on('connection', async (socket) => {
         u.id = idGeral;
         u.socketId = socket.id;
         console.log(`User connected: Name: ${u.name} - ID: ${u.id} - PUBLIC KEY: ${u.publicKey} - SOCKET ID: ${u.socketId}`);
-        let chatUser = new User(u.id, u.name, u.socketId, u.publicKey);
-        users.push(chatUser);
         let sockets = Object.keys(io.sockets.connected);
         console.log(`Usuários conectados: ${sockets.length}`)
         sockets.forEach(socket => {
@@ -49,6 +47,14 @@ io.on('connection', async (socket) => {
 
     // Procedimento ao iniciar um chat
     socket.on('startChat', async (userChatInfo) => {
+        let chatUser = new User(userChatInfo.id, userChatInfo.name, userChatInfo.socketId, userChatInfo.publicKey);
+        users.push(chatUser);
+        users.forEach(user => {
+            console.log('-------');
+            console.log('ID: ' + user.id);
+            console.log('Name: ' + user.name);
+            console.log('SocketID: ' + user.socketId);
+        })
         if (!chatStarted) {
             chatStarted = true;
             chatHoster.id = userChatInfo.id;
@@ -84,42 +90,57 @@ io.on('connection', async (socket) => {
     socket.on('disconnect', async () => {
         console.log('Usuario desconectado - SocketId: ' + socket.id);
         let sockets = Object.keys(io.sockets.connected);
+        // Número de usuários ainda conectados
         console.log('Usuários ainda conectados: ' + sockets.length);
+        // Lista usuários conectados pelo socket
+        console.log('++++++++++++++++');
         sockets.forEach(socket => {
             console.log(`SocketId: ${socket}`);
         })
-
-        if (!sockets.length) {
-            console.log('Chat encerrado');
-            chatHoster.id = -1;
-            chatHoster.name = '';
-            chatHoster.socketId = '';
-            chatStarted = false;
-            idGeral = 0;
-        } else {
-            if (chatHoster.socketId == socket.id) {
-                // Novo chatHoster
-                if (users.length > 1) {
-                    chatHoster.id = users[1].id;
-                    chatHoster.name = users[1].name;
-                    chatHoster.socketId = users[1].socketId;
-                } else {
-                    chatHoster.id = users[0].id;
-                    chatHoster.name = users[0].name;
-                    chatHoster.socketId = users[0].socketId;
-                }
-                sendNewChatHoster(chatHoster);
+        console.log('++++++++++++++++');
+        // Remove da lista de users o usuário que acabou de sair
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].socketId == socket.id) {
+                await sendUserDisconnect(users[i]);
+                users.splice(i, 1);
             }
-            for (let i = 0; i < users.length; i++) {
-                if (users[i].socketId == socket.id) {
-                    await sendUserDisconnect(users[i]);
-                    users.splice(i, 1);
-                }
-            }
-
         }
 
+        // Se não tiver mais ninguém conectado, reinicializa o chat
+        if (!sockets.length) {
+            endChat();
+        } else {
+            // Caso ainda tenha usuário conectado, verifica se foi o hoster que saiu
+            // caso tenha sido o hoster a sair, tem trocar
+            if (chatHoster.socketId == socket.id) {
+                // Confirmando que aind atem usuário ativo
+                if (sockets.length && users.length) {
+                    // Busca pelo novo hoster
+                    let found = users.find((element) => {
+                        return element.socketId == sockets[0];
+                    })
+                    // Se encontrar o novo hoster, coloca-o como hoster
+                    if (found) {
+                        chatHoster.id = found.id;
+                        chatHoster.name = found.name;
+                        chatHoster.socketId = found.socketId;
+                        sendNewChatHoster(chatHoster);
+                    } else {
+                        // Caso não encontre, coloca o primeiro no array users
+                        chatHoster.id = users[0].id;
+                        chatHoster.name = users[0].name;
+                        chatHoster.socketId = users[0].socketId;
+                        sendNewChatHoster(chatHoster);
+                    }
+
+                } else {
+                    // Se o sockets ou users estiver zerado, finaliza o chat
+                    endChat()
+                }
+            }
+        }
     })
+
     socket.on('typing', (id) => {
         io.emit('someTyping', id);
     })
@@ -149,6 +170,19 @@ function sendUserDisconnect(userDisc) {
 function sendNewChatHoster(newHosterInfo) {
     io.emit('newHost', newHosterInfo);
 }
+
+function endChat() {
+    console.log('Chat encerrado');
+    chatHoster.id = -1;
+    chatHoster.name = '';
+    chatHoster.socketId = '';
+    chatStarted = false;
+    users = [];
+    if (idGeral >= 999999999) {
+        idGeral = 0;
+    }
+}
+
 var appPort = (process.env.PORT || 4444);
 http.listen(appPort, () => {
     console.log(`Listen to the port ${appPort}`);
